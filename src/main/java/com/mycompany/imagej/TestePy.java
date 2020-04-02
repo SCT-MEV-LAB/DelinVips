6a7ea76e6a73410545acd06fc1ada75d61d0cd1e
Nancy Baygorrea <nbaygorrea@gmail.com>
2020-04-01 21:54:21 -0300

    Criação do projeto Delin Vips
    Realizado commit com os arquivos no repositorio do Projeto

---
+++ src/main/java/com/mycompany/imagej/TestePy.java
@@ -0,0 +1,136 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package com.mycompany.imagej;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class TestePy implements PlugInFilter {
+	protected ImagePlus image;
+	protected double valor;
+	protected String env;
+	protected String property = "java.io.tmpdir"; 
+	protected String tempDir = System.getProperty(property);
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about")) {
+			showAbout();
+			return DONE;
+		}
+
+		image = imp;
+		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
+	}
+
+	@Override
+	public void run(ImageProcessor ip) {
+		try {
+			
+			
+			IJ.save(tempDir+File.separator+"some-image.jpg");
+
+			String testeStrings = "import sys\n" + "import pyvips\n" + "import logging     \n"
+					+ "LOG_FILENAME = 'log.txt'\n" + "logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)\n"
+					+ "#logging.basicConfig(level = logging.DEBUG)\n" + "#            \n" + "#   \n" + "#    \n"
+					+ "#           \n" + "# threshold ... why not\n" + "try:\n" + "	T = float(sys.argv[3])\n" + "\n"
+					+ "	ps = pyvips.Image.new_from_file(sys.argv[1], access='sequential')\n" + "	 \n"
+					+ "	# window size (neighbourhood) we search over\n" + "	window_size = 3\n" + "		    \n"
+					+ "	# find the max and min for each window\n" + "	ps_min = ps.rank(window_size, window_size, 0)\n"
+					+ "	ps_max = ps.rank(window_size, window_size, window_size * window_size - 1)\n" + "	  \n"
+					+ "	# difference   \n" + "	D = ps_max - ps_min\n" + "\n" + "	# for border pixels,\n"
+					+ "	border = (ps - ps_min < ps_max - ps).ifthenelse(ps_min, ps_max)\n" + "	  \n"
+					+ "	out_ps = (D < T).ifthenelse(ps, border)\n" + "		\n" + "		  \n" + "\n"
+					+ "	out_ps.write_to_file(sys.argv[2])\n" + "\n" + "except:\n"
+					+ "    logging.exception('Pegamos um erro aqui')\n" + "    raise";
+
+			File file = new File(tempDir+File.separator+"config-delin.txt");
+
+			if (file.exists()) {
+				FileReader arq = new FileReader(file);
+				BufferedReader lerArq = new BufferedReader(arq);
+				env = lerArq.readLine();
+				lerArq.close();
+			}
+
+			BufferedWriter out2 = new BufferedWriter(new FileWriter(tempDir+File.separator+"testStrings.py"));
+			out2.write(testeStrings);
+			out2.close();
+			if (showDialog()) {
+				Process p1 = Runtime.getRuntime()
+						.exec(env + "python "+tempDir+File.separator+"testStrings.py "+tempDir+File.separator+"some-image.jpg "+tempDir+File.separator+"x.jpg " + valor);
+				Process p2 = Runtime.getRuntime()
+						.exec(env + "python3 "+tempDir+File.separator+"testStrings.py  "+tempDir+File.separator+"some-image.jpg "+tempDir+File.separator+"x.jpg " + valor);
+				p1.waitFor();
+				p2.waitFor();
+
+				ImagePlus imagemNova = IJ.openImage(tempDir+File.separator+"x.jpg");
+				imagemNova.show();
+
+				IJ.showMessage("executado sem erro");
+				BufferedWriter out = new BufferedWriter(new FileWriter(tempDir+File.separator+"config-delin.txt"));
+				out.write(env);
+				out.close();
+			}
+		} catch (IOException | InterruptedException e) {
+			IJ.showMessage(e.getMessage());
+		}
+		image.updateAndDraw();
+
+	}
+
+	private boolean showDialog() {
+		GenericDialog gd = new GenericDialog("Valor QQ");
+
+		gd.addNumericField("valor QQ", 0.00, 2);
+		gd.addStringField("selecionar env", env);
+
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return false;
+
+		// get entered values
+		valor = gd.getNextNumber();
+		env = gd.getNextString();
+
+		return true;
+	}
+
+	public void showAbout() {
+		IJ.showMessage("Teste Python", "Teste execução chamada de script py");
+	}
+
+	public static void main(String[] args) throws Exception {
+		Class<?> clazz = TestePy.class;
+		java.net.URL url = clazz.getProtectionDomain().getCodeSource().getLocation();
+		java.io.File file = new java.io.File(url.toURI());
+		System.setProperty("plugins.dir", file.getAbsolutePath());
+
+		// start ImageJ
+		new ImageJ();
+
+		// open the Clown sample
+		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
+		image.show();
+
+		// run the plugin
+		IJ.runPlugIn(clazz.getName(), "");
+	}
+}
